6e94595baac73e58fbf31c9fb09a68a8dc06ad5c
Alex Ciocartau-EXT <alex.ciocartau-ext@continental-corporation.com>
2022-05-09 19:51:05 +0300

    finished implementation of tcp/ip clients

--- src/app/HivesConnectionsManager.cpp
+++ src/app/HivesConnectionsManager.cpp
@@ -2,7 +2,9 @@
 
 HivesConnectionsManager::HivesConnectionsManager(): QObject()
 {
-
+    QObject::connect(&this->tcpServer, SIGNAL(TcpClientConnected(QTcpSocket *)), this, SLOT(TcpClientConnected(QTcpSocket *)));
+    QObject::connect(&this->tcpServer, SIGNAL(TcpClientDisconnected(QTcpSocket *)), this, SLOT(TcpClientDisconnected(QTcpSocket *)));
+    QObject::connect(&this->tcpServer, SIGNAL(TcpClientDataReception(QTcpSocket *)), this, SLOT(TcpClientDataReception(QTcpSocket *)));
 }
 
 HivesConnectionsManager::~HivesConnectionsManager()
@@ -10,11 +12,32 @@ HivesConnectionsManager::~HivesConnectionsManager()
 
 }
 
-void HivesConnectionsManager::StartListen(const QHostAddress &address, quint16 port)
+bool HivesConnectionsManager::StartListen(const QHostAddress &address, quint16 port)
 {
-
+    return this->tcpServer.StartListening(address, port);
 }
 void HivesConnectionsManager::StopListen()
 {
+    this->tcpServer.StopListening();
+}
 
+void HivesConnectionsManager::TcpClientConnected(QTcpSocket *client)
+{
+    emit this->ActiveConnectionsCountChanged(this->tcpServer.GetConnectionsCount());
+}
+
+void HivesConnectionsManager::TcpClientDisconnected(QTcpSocket *client)
+{
+    //qDebug() << "[HIVE] client disconnected";
+
+
+    emit this->ActiveConnectionsCountChanged(this->tcpServer.GetConnectionsCount());
+}
+
+void HivesConnectionsManager::TcpClientDataReception(QTcpSocket *client)
+{
+
+    //qDebug() << "[HIVE] Data reception available";
+
+    emit this->OnDataPacketAvailable(  client->peerAddress(), client->readAll() );
 }
